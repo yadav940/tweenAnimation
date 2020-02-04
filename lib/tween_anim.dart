01ea5efe262031205bfe5ef2c7f351d166ae56e2
vinodyadav <vinod@modistabox.com>
2020-02-04 18:36:53 +0530

    asset icon tween
    Signed-off-by: vinodyadav <vinod@modistabox.com>

--- lib/tween_anim.dart
+++ lib/tween_anim.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TweenAnim extends StatefulWidget {
   @override
   _TweenAnimState createState() => _TweenAnimState();
@@ -8,35 +7,57 @@ class TweenAnim extends StatefulWidget {
 
 class _TweenAnimState extends State<TweenAnim> {
   double targetValue = 24.0;
-  Color _colorValue=Colors.black;
-  
+  Color _colorValue = Colors.black;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-
         margin: EdgeInsets.symmetric(vertical: 100),
         child: Center(
           child: TweenAnimationBuilder(
             //tween: Tween<double>(begin: 0, end: targetValue),
-            tween: ColorTween(begin: Colors.black,end: Colors.orange),
+            tween: ColorTween(begin: Colors.orange, end: Colors.red),
             curve: Curves.bounceInOut,
-            onEnd: ()=>(){
+            onEnd: () => () {
               print("all end");
+              _colorValue = Colors.orange;
+              setState(() {
+                debugPrint('on click of image change');
+                //==Colors.orange?Colors.orange:Colors.red;
+                //targetValue = targetValue == 24.0 ? 48.0 : 24.0;
+              });
             },
             duration: Duration(seconds: 2),
             builder: (BuildContext context, Color _Value, Widget child) {
-              return IconButton(
+              /*return IconButton(
                 // iconSize: size,
                 color: _colorValue,
-                icon: child,
-                //icon: Image.asset('assets/user.png'),
+                //icon: child,
+                icon: Image.asset('assets/user.png'),
                 onPressed: () {
+                  _colorValue = _Value;
                   setState(() {
-                    _colorValue=_Value;//==Colors.orange?Colors.orange:Colors.red;
+                    debugPrint('on click of image change');
+                    //==Colors.orange?Colors.orange:Colors.red;
                     //targetValue = targetValue == 24.0 ? 48.0 : 24.0;
                   });
                 },
+              );*/
+              debugPrint('image color -- $_Value');
+              return InkWell(
+                onTap: () {
+                  _colorValue = Colors.orange;
+                  setState(() {
+                    debugPrint('on click of image change');
+                    //==Colors.orange?Colors.orange:Colors.red;
+                    //targetValue = targetValue == 24.0 ? 48.0 : 24.0;
+                  });
+                },
+                child: Image.asset(
+                  'assets/user.png',
+                  color: _Value,
+                ),
               );
             },
             child: Icon(Icons.sentiment_satisfied),
